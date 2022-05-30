0ef7b426391610176538344cf4a8c10f0e749cd9
SnowCarter <snowdwq@gmail.com>
2022-05-29 23:29:17 -0400

    spot dataset

--- src/scanRegistration.cpp
+++ src/scanRegistration.cpp
@@ -476,7 +476,7 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/points_raw", 100, laserCloudHandler);
+    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/os_cloud_node/points", 100, laserCloudHandler);
 
     pubLaserCloud = nh.advertise<sensor_msgs::PointCloud2>("/velodyne_cloud_2", 100);
 
